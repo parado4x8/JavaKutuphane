189962ae1cb9f0bad36ffa88a3e297c602e425a1
Denizcan Demir <denizcan.demir@ozguryazilim.com.tr>
2018-08-13 19:40:36 +0300

    login form düzenlendi

--- src/main/java/com/deniz/kutuphane/security/JavaSecurity.java
+++ src/main/java/com/deniz/kutuphane/security/JavaSecurity.java
@@ -1,24 +1,35 @@
 package com.deniz.kutuphane.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class JavaSecurity extends WebSecurityConfigurerAdapter {
-	
+
+	@Autowired
+	DataSource dataSouce;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(auth);
+		auth.jdbcAuthentication().dataSource(dataSouce).passwordEncoder(getPasswordEncoder());
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/login").permitAll();
 		http.authorizeRequests().anyRequest().authenticated();
 		http.formLogin().loginPage("/login").loginProcessingUrl("/form/login").failureUrl("/login?loginFailed=true");
+	}
+	
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
