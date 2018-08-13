69987d54820ee88dca2d457e328f428f0dc1f91e
Denizcan Demir <denizcan.demir@ozguryazilim.com.tr>
2018-08-13 18:05:21 +0300

    initial commit

---
+++ src/main/java/com/deniz/kutuphane/JavaKutuphaneApplication.java
@@ -0,0 +1,12 @@
+package com.deniz.kutuphane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKutuphaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaKutuphaneApplication.class, args);
+	}
+}
---
+++ src/test/java/com/deniz/kutuphane/JavaKutuphaneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.deniz.kutuphane;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaKutuphaneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
