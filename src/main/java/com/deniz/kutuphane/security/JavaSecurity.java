96aa67375743259eaf5d47282c414ddae6ed1e3e
Denizcan Demir <denizcan.demir@ozguryazilim.com.tr>
2018-08-13 18:52:23 +0300

    layout eklendi

--- src/main/java/com/deniz/kutuphane/security/JavaSecurity.java
+++ src/main/java/com/deniz/kutuphane/security/JavaSecurity.java
@@ -16,8 +16,9 @@ public class JavaSecurity extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
+		http.authorizeRequests().antMatchers("/login").permitAll();
+		http.authorizeRequests().anyRequest().authenticated();
+		http.formLogin().loginPage("/login").loginProcessingUrl("/form/login").failureUrl("/login?loginFailed=true");
 	}
 
 }
